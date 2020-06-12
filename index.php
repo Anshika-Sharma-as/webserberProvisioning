fbf3a7126735301aff05894ec1a850f859a47097
Anshika-Sharma-as <52372525+Anshika-Sharma-as@users.noreply.github.com>
2020-06-12 13:52:51 +0530

    first_commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<body bgcolor='aqua'>
+
+<pre>
+
+<?php 
+
+<h1> print "Welcome to this page that will show this OS details!!!!"</h1>
+
+print `ifconfig`;
+
+print "\n ";
+
+print `lscpu`;
+
+print `free -m`;
+
+?>
+
+</pre>
+
+</body>
