9ecd419871d7edb1ff3a62d358b2ad46225703c9
Anshika-Sharma-as <52372525+Anshika-Sharma-as@users.noreply.github.com>
2020-08-21 20:23:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 
 <body bgcolor='aqua'>
  <h1> Welcome to this integration </h1>
- <img src="https://d1f82gi6qn2ums.cloudfront.net/logo.jpg"></img> 
+ <img src="https://hashicorp.github.io/field-workshops-terraform/slides/aws/terraform-oss/images/tf_aws.png"></img> 
 <pre>
 
-<?php
-  $cloudfront_url = `head -n1 mydesti.txt`;
-  $img_path = "https://".$cloudfront_url."/logo.jpg";
-  echo "<br>";
-  echo "<img src='{$img_path}' width=100 height=100>";
+<?php 
+ print "Welcome to this page that will show this OS details!!!!";
+print `ifconfig`;
+print "\n ";
+print `lscpu`;
+print `free -m`;
 ?>
 
 </pre>
